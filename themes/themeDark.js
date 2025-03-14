ba60118f61dcaed4e1edb908f7ff182d19b96db0
kroot loops <132160403+sskriptz@users.noreply.github.com>
2025-03-14 15:26:42 -0400

    Update themeDark.js

--- themes/themeDark.js
+++ themes/themeDark.js
@@ -1,4 +1,41 @@
 export function applyDarkTheme() {
     appearancePanel.style.backgroundColor = '#1e293b';
-    document.body.style.backgroundColor = '#0f172a';
+
+
+    particleContainer.style.backgroundColor = '#1a1a1a';
+    particleContainer.style.color = '#ffffff';
+    particlesButtonRain.style.backgroundColor = '#333333';
+    particlesButtonRain.style.color = '#ffffff';
+    particlesButtonSnow.style.backgroundColor = '#333333';
+    particlesButtonSnow.style.color = '#ffffff';
+
+    weatherContainer.style.backgroundColor ='#1a1a1a';
+    weatherContainer.style.color = '#ffffff';
+    weatherContainer.style.borderColor = '#333333';
+    weatherButtonsStorm.style.backgroundColor = '#333333';
+    weatherButtonsStorm.style.color = '#ffffff';
+    weatherButtonsBlizzard.style.backgroundColor = '#333333';
+    weatherButtonsBlizzard.style.color = '#ffffff';
+
+    wallpaperContainer.style.backgroundColor = '#1a1a1a';
+    wallpaperContainer.style.color = '#ffffff';
+    wallpaperBg1Btn.style.backgroundColor = '#333333';
+    wallpaperBg1Btn.style.color = '#ffffff';
+    wallpaperBg2Btn.style.backgroundColor = '#333333';
+    wallpaperBg2Btn.style.color = '#ffffff';
+    wallpaperBg3Btn.style.backgroundColor = '#333333';
+    wallpaperBg3Btn.style.color = '#ffffff';
+    wallpaperBg4Btn.style.backgroundColor = '#333333';
+    wallpaperBg4Btn.style.color = '#ffffff';
+
+    audioPlayer.style.backgroundColor = '#1a1a1a';
+
+    profileContainer.style.backgroundColor = '#1a1a1a';
+
+    profileEditBtnContainer.style.backgroundColor = "#1a1a1a";
+    
+    profileSettingsBtnContainer.style.backgroundColor = "#1a1a1a";
+
+    tdContainer.style.backgroundColor = "#1a1a1a";
+    tdHeader.style.color = "#ffffff";
 }
