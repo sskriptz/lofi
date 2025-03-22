015f40e33e3dd74b57fdf60986ac4a7b9d7cccb0
kroot loops <132160403+sskriptz@users.noreply.github.com>
2025-03-22 18:18:11 -0400

    Update songs.js

--- songs.js
+++ songs.js
@@ -3,7 +3,6 @@ const songs = [
     // { title: "BURN", artist: "Kanye West, Ty Dolla $ign", src: "https://www.dropbox.com/scl/fi/kptzdit2rgx87d1uizdzb/09-Burn.mp3?rlkey=ukc7xnuxbvr1epg27ps2dg5be&st=py6tbav3&raw=1" },
     // { title: "Magnolia", artist: "Playboi Carti", src: "https://www.dropbox.com/scl/fi/91dvrw5bzro2p89fddcyf/spotifydown.com-Magnolia-Playboi-Carti.mp3?rlkey=z6zq0vva6dimgy39ywilvtuwf&st=3l1tw5pz&raw=1" },
     // { title: "IDGAF", artist: "BoyWithUke, blackbear", src: "https://www.dropbox.com/scl/fi/5c858z7y3ouz8nqem6jxd/spotifydown.com-IDGAF-with-blackbear-BoyWithUke.mp3?rlkey=1fa4hysag7ggge2u6sh5nyawh&st=m0l0ao9o&raw=1" },
-    // { title: "Save Your Tears", artist: "The Weeknd", src: "https://www.dropbox.com/scl/fi/20acf6hqe3zrbb4ywg9v4/spotifydown.com-Save-Your-Tears-The-Weeknd.mp3?rlkey=klhtvkr7f4wwuzwnui1a2bwzi&st=edx0mba0&raw=1" },
     // { title: "Kerosene", artist: "Crystal Castles", src: "https://www.dropbox.com/scl/fi/pssm1vnhnf6yfbkzuz9yf/spotifydown.com-Kerosene-Crystal-Castles.mp3?rlkey=xzbkqsfjxhq8eyrv91nbchiv3&st=05674r11&raw=1"},
     // { title: "3D OUTRO", artist: "LUCKI", src: "https://www.dropbox.com/scl/fi/gh1o71q4315gzhof9l45l/spotifydown.com-3D-Outro-LUCKI.mp3?rlkey=bfrc47lgyt4d1y6w7mc1pfw2q&st=fndbxgh7&raw=1"},
     // { title: "Diamonds (feat. Gunna)", artist: "Young Thug, Gunna", src: "https://www.dropbox.com/scl/fi/q0sexnbnjgssrwy6xqack/spotifydown.com-Diamonds-feat.-Gunna-Young-Thug.mp3?rlkey=x7v1rwuc3j5tu12xt7u0riwps&st=bdwwpevw&raw=1" },
@@ -25,7 +24,12 @@ const songs = [
     { title: "Aria Math", artist: "C418", src: "https://www.dropbox.com/s/71npzaqei68woi7/spotidownloader.com%20-%20Aria%20Math%20-%20C418.mp3?st=psq1h1bo&raw=1" },
     { title: "Sahara", artist: "Hensonn", src: "https://www.dropbox.com/s/wuhctp0n48n2qw0/spotidownloader.com%20-%20Sahara%20-%20Hensonn.mp3?st=jr2996aw&raw=1" },
     { title: "Memory Reboot - Slowed", artist: "VØJ, Narvent", src: "https://www.dropbox.com/s/a4d8877gjcn8wli/spotidownloader.com%20-%20Memory%20Reboot%20-%20Slowed%20-%20V%C3%98J.mp3?st=0kz25s2i&raw=1" },
+    { title: "Save Your Tears", artist: "The Weeknd", src: "https://www.dropbox.com/scl/fi/20acf6hqe3zrbb4ywg9v4/spotifydown.com-Save-Your-Tears-The-Weeknd.mp3?rlkey=klhtvkr7f4wwuzwnui1a2bwzi&st=pe6fiyir&raw=1" },
+    { title: "Flamin Hot Cheetos (slowed + reverb)", artist: "Clairo", src: "https://www.dropbox.com/scl/fi/vcf10gqhedr364nhl8lxd/clairo-flamin-hot-cheetos-slowed-reverb-TubeRipper.com.mp3?rlkey=tvodjy8uqsbm1vu8w61dv4iao&st=rbhda1b4&raw=1" },
+    { title: "Lovely", artist: "Eric Godlow", src: "https://www.dropbox.com/scl/fi/p8njklpse4gvlzkl7l3n4/FREE-Lo-fi-Type-Beat-Lovely-TubeRipper.com.mp3?rlkey=q3ihtfa5fn8b914d1vqwxq5ed&st=nws4d2qz&raw=1" },
+    { title: "You're Never Around", artist: "Eric Godlow", src: "https://www.dropbox.com/scl/fi/9zoajlj7bmyfcij7iqwmi/FREE-Lo-fi-Type-Beat-Youre-never-around-TubeRipper.com.mp3?rlkey=43i4mz311ehjvndj2hag6c99e&st=4ciwd2ym&raw=1" },
 ];
+
 
 
 export default songs;
