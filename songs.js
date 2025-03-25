883b714c014b6aaca59fb5a113c205fa53e283c4
kroot loops <132160403+sskriptz@users.noreply.github.com>
2025-03-25 02:21:59 -0400

    Update songs.js

--- songs.js
+++ songs.js
@@ -1,6 +1,5 @@
 const songs = [
     // { title: "CARNIVAL", artist: "Kanye West, Ty Dolla $ign", src: "https://www.dropbox.com/scl/fi/jdm2pc5i0z4tssbtf6t2c/12-Carnival.mp3?rlkey=n9ncc8egcnwes2a9bw5660dky&st=cn0g5e0o&raw=1" },
-    // { title: "BURN", artist: "Kanye West, Ty Dolla $ign", src: "https://www.dropbox.com/scl/fi/kptzdit2rgx87d1uizdzb/09-Burn.mp3?rlkey=ukc7xnuxbvr1epg27ps2dg5be&st=py6tbav3&raw=1" },
     // { title: "Magnolia", artist: "Playboi Carti", src: "https://www.dropbox.com/scl/fi/91dvrw5bzro2p89fddcyf/spotifydown.com-Magnolia-Playboi-Carti.mp3?rlkey=z6zq0vva6dimgy39ywilvtuwf&st=3l1tw5pz&raw=1" },
     // { title: "IDGAF", artist: "BoyWithUke, blackbear", src: "https://www.dropbox.com/scl/fi/5c858z7y3ouz8nqem6jxd/spotifydown.com-IDGAF-with-blackbear-BoyWithUke.mp3?rlkey=1fa4hysag7ggge2u6sh5nyawh&st=m0l0ao9o&raw=1" },
     // { title: "Kerosene", artist: "Crystal Castles", src: "https://www.dropbox.com/scl/fi/pssm1vnhnf6yfbkzuz9yf/spotifydown.com-Kerosene-Crystal-Castles.mp3?rlkey=xzbkqsfjxhq8eyrv91nbchiv3&st=05674r11&raw=1"},
@@ -29,6 +28,10 @@ const songs = [
     { title: "Lovely", artist: "Eric Godlow", src: "https://www.dropbox.com/scl/fi/p8njklpse4gvlzkl7l3n4/FREE-Lo-fi-Type-Beat-Lovely-TubeRipper.com.mp3?rlkey=q3ihtfa5fn8b914d1vqwxq5ed&st=nws4d2qz&raw=1" },
     { title: "You're Never Around", artist: "Eric Godlow", src: "https://www.dropbox.com/scl/fi/9zoajlj7bmyfcij7iqwmi/FREE-Lo-fi-Type-Beat-Youre-never-around-TubeRipper.com.mp3?rlkey=43i4mz311ehjvndj2hag6c99e&st=4ciwd2ym&raw=1" },
     { title: "Cry For Me", artist: "The Weeknd", src: "https://www.dropbox.com/scl/fi/szai6ueone4k2hx9ktz5u/Cry-For-Me-The-Weeknd.mp3?rlkey=phi8k5gzet1kldjww9xivsc0u&st=8zxvpa33&raw=1" },
+    { title: "BURN", artist: "Kanye West, Ty Dolla $ign", src: "https://www.dropbox.com/scl/fi/kptzdit2rgx87d1uizdzb/09-Burn.mp3?rlkey=ukc7xnuxbvr1epg27ps2dg5be&st=py6tbav3&raw=1" },
+    { title: "Better in the Dark", artist: "Jordana, TV Girl", src: "https://www.dropbox.com/scl/fi/76acxhkpskzo8e35tjxra/Better-in-the-Dark-Jordana.mp3?rlkey=jm6m2jyqkcx7e0yas944rjtui&st=3zq97guw&raw=1" },
+    { title: "Taking What's Not Yours", artist: "TV Girl", src: "https://www.dropbox.com/scl/fi/85f0k35a01oez3x2oew75/Taking-What-s-Not-Yours-TV-Girl.mp3?rlkey=omecdlykgkb8s26q4kanth1qm&st=s252iy7d&raw=1" },
+    { title: "Not Allowed", artist: "TV Girl", src: "https://www.dropbox.com/scl/fi/l7qd2zztyx9e5r6vkzyhb/Not-Allowed-TV-Girl.mp3?rlkey=fz8jjb06hmushd5dn27gzi7c2&st=g5cclm68&raw=1" },
 ];
 
 
