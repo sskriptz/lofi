7857f475052ab8b6920d292dfe7253a9fd25222a
kroot loops <132160403+sskriptz@users.noreply.github.com>
2025-03-26 13:33:23 -0400

    Update store.js

--- fun/store.js
+++ fun/store.js
@@ -1,41 +1,7 @@
 import { getAuth, getFirestore } from '../firebase/firebase-config.js';
 import { formatCoins } from './coins.js';
+import STORE_ITEMS from './store-items.js';
 
-// Define store items with online images and categories
-const STORE_ITEMS = [
-    {
-        id: 'theme-1',
-        name: 'Cyberpunk Neon',
-        description: 'Immersive cyberpunk-themed interface',
-        price: 100,
-        category: 'Themes',
-        image: 'https://i.etsystatic.com/22439432/r/il/c95905/4402318272/il_fullxfull.4402318272_36aj.jpg'
-    },
-    {
-        id: 'music-1',
-        name: 'Tokyo Rain Beats',
-        description: 'Soothing lo-fi track with city ambiance',
-        price: 150,
-        category: 'Music',
-        image: 'https://images.unsplash.com/photo-1516450360452-9312f5e86fc7?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHwxfHxsb2ZpJTIwbXVzaWN8ZW58MHx8fHwxNzExMzYyNDAwfDA&ixlib=rb-4.0.3&q=80&w=1080'
-    },
-    {
-        id: 'effect-1',
-        name: 'Cosmic Particle Effect',
-        description: 'Mesmerizing space-themed animation',
-        price: 200,
-        category: 'Profile Effects',
-        image: 'https://images.unsplash.com/photo-1614730321146-b6fa6a46bcb4?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHwxfHxjb3NtaWMlMjBwYXJ0aWNsZXN8ZW58MHx8fHwxNzExMzYyNDAwfDA&ixlib=rb-4.0.3&q=80&w=1080'
-    },
-    {
-        id: 'background-1',
-        name: 'Synthwave Cityscape',
-        description: 'Retro-futuristic background animation',
-        price: 250,
-        category: 'Backgrounds',
-        image: 'https://i.redd.it/yxuve5xdpmq91.jpg'
-    }
-];
 
 export function initializeStore() {
     const auth = getAuth();
